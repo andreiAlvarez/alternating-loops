e8dd3fe4c62c76f719f24da3f1bd3681a3d4ee0e
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-10-15 22:13:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // solution 1
 
+
 function combine(...arr) {
   let newA = [];
-  let max = Math.max(...arr.map(v => v.length));
-  for (let i = 0; i < max; i++) {
+  let num = Math.max(...arr.map(v => v.length));
+  for (let i = 0; i < num; i++) {
     for (let j = 0; j < arr.length; j++) {
       arr[j][i] ? newA.push(arr[j][i]) : 1;
     }
   }
-  return newArr;
+  return newA;
 }
+
+// Solution 2
+
+const combine = (...a) => 
+  a.reduce((arr, v, i) => 
+    (v.forEach((v, j) => 
+      arr[j * a.length + i] = v), arr), [])
+    .filter(v => v != undefined)
